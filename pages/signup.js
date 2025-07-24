71cf26f1297bba95dbdd948f605ae426022228b1
sooyeon <sy23lee@gmail.com>
2025-07-24 22:13:02 +0900

    "feat:회원가입 페이지 UI (WIP)"

---
+++ pages/signup.js
@@ -0,0 +1,17 @@
+import Button from "@/components/Button";
+
+export default function SignUp() {
+  return(
+    <div>
+      <div>
+
+      <div> logo</div>
+      <div>login</div>
+      <form />
+      <Button/>
+
+      </div>
+
+    </div>
+  )
+}
